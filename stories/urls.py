cb4f1ce3219cd3c6bdf73a6b3f9c34f3542bb71a
ZakiaHussaini <zakiahussaini19@gmail.com>
2023-08-05 14:35:41 +0430

    add StoryDetail View to Stories app

--- stories/urls.py
+++ stories/urls.py
@@ -4,4 +4,6 @@ from stories import views
 
 urlpatterns = [
     path('stories/', views.StoryList.as_view()),
+    path('stories/<int:pk>/', views.StoryDetail.as_view()),
+    
 ]
--- stories/views.py
+++ stories/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Story
 from .serializers import StorySerializer
+from django.http import Http404
+from story.permissions import IsOwnerOrReadOnly
 
 
 class StoryList(APIView):
@@ -29,4 +31,43 @@ class StoryList(APIView):
         return Response(
             serializer.errors, status = status.HTTP_400_BAD_REQUEST
         )
+        
+class StoryDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = StorySerializer
+    
+    def get_object(self, pk):
+        try:
+            story = Story.objects.get(pk=pk)
+            self.check_object_permissions(self.request, story)
+            return story
+        except Story.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        story = self.get_object(pk)
+        serializer = StorySerializer(
+            story, context={'request': request}
+        )
+        return Response(serializer.data)
+    
+    
+    def put(self, request, pk):
+        story = self.get_object(pk)
+        serializer = StorySerializer(
+            story, data=request.data, context = {'request':request}
+        )
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST 
+        )
             
+    def delete(self, request, pk):
+        story = self.get_object(pk)
+        story.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
