acb844e316287069767b8d3f83a8bb83fc0bf105
ZakiaHussaini <zakiahussaini19@gmail.com>
2023-08-15 14:37:15 +0430

    connect to elephantSQL database

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os
+os.environ['DATABASE_URL'] = 'postgres://ljjlxzwg:WJmzHLUsTNxF_ZZ4gCZ9AXJUene7jBNW@trumpet.db.elephantsql.com/ljjlxzwg'
 os.environ['CLOUDINARY_URL'] = 'cloudinary://619146312833525:w8-CWIKW3AunltlqYqybGIcuBow@dd5s8l3yh'
-os.environ['DEV'] = '1'
